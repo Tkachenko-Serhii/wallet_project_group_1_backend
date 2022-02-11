b4ea30b0a4a4092081917ea05d0123444106042f
Serhii Tkachenko <79119130+Tkachenko-Serhii@users.noreply.github.com>
2022-02-12 00:47:01 +0200

    Initial commit

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const { Developer } = require('./developer');
+const { Transaction } = require('./transaction');
+const { User } = require('./user');
+
+module.exports = {
+  Developer,
+  Transaction,
+  User,
+};
